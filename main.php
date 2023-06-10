a13f3da94734f14a59ae7c9c3a879c7f93132ebb
Antoine <125296602+PoirierTheGreat0440E@users.noreply.github.com>
2023-06-10 23:33:59 +0200

    Main web page + stylesheet
    The code for the main page of the website!
    There's also the associated stylesheet so that it doesn't look like an empty shell.
    Still familiarizing myself with the intricacies of CSS...

---
+++ main.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8"/>
+	<title> Enhanced Yahoos </title>
+	<meta name="description" content="Here's Anthony's personal webpage !"/>
+	<meta name="author" content="Poirier Antoine"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="CSS/stylesheet1.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+	<!-- The header of the website -->
+	<header id="Header_part">
+
+		<p>This is a header</p>
+
+	</header>
+
+	<main>
+
+		<!-- The nav of the website -->
+		<nav>
+			<table id="Nav_table">
+				<th>Forum</th>
+				<th>Images</th>
+				<th>Videos</th>
+				<th>About Us</th>
+			</table>
+		</nav>
+
+		<div id="Main_part">
+			<p>This is the main part of the website</p>
+		</div>
+
+		<!-- The footer of the website -->
+		<footer id="Footer_part">
+			<p>This is a footer</p>
+		</footer>
+
+	</main>
+
+</body>
+
+</html>
